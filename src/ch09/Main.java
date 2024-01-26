a41f36ea87a227786157c54abe7a1333fe7e7ba0
Hoshyar Karimi <hoshyarkarimi@outlook.com>
2024-01-26 23:14:56 +0330

    super()

--- src/ch09/Main.java
+++ src/ch09/Main.java
@@ -3,5 +3,7 @@ package ch09;
 public class Main {
     public static void main(String[] args) {
         Duck duck = new Duck(42);
+        Hippo h = new Hippo("Buffy");
+        System.out.println(h.getName());
     }
 }
