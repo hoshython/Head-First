33937be12e2555bf11c7e253cb108c87a1b7374b
Hoshyar Karimi <hoshyarkarimi@outlook.com>
2024-02-01 14:33:17 +0330

    chapter 14

---
+++ src/ch14/SimpleGui2.java
@@ -0,0 +1,25 @@
+package ch14;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleGui2 implements ActionListener {
+    private JButton button;
+    
+    public void go() {
+        JFrame frame = new JFrame("Simple GUI 2");
+        button = new JButton("click me");
+        
+        button.addActionListener(this);
+        
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+    
+    @Override public void actionPerformed(ActionEvent event) {
+        button.setText("I've been clicked!");
+    }
+}
---
+++ src/ch14/SimpleGuiRunner2.java
@@ -0,0 +1,8 @@
+package ch14;
+
+public class SimpleGuiRunner2 {
+    public static void main(String[] args) {
+        SimpleGui2 gui = new SimpleGui2();
+        gui.go();
+    }
+}
